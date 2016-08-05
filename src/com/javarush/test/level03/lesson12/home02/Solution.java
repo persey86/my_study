9eca483206e2f6d14f6fa116b0620f31a76d72a5
persey86 <persey86@ukr.net>
2016-08-05 17:09:32 +0300

    Update Solution.java

--- src/com/javarush/test/level03/lesson12/home02/Solution.java
+++ src/com/javarush/test/level03/lesson12/home02/Solution.java
@@ -1,8 +1,3 @@
-package com.javarush.test.level03.lesson12.home02;
-
-/* Я не хочу изучать Java, я хочу большую зарплату
-Вывести на экран десять раз надпись «Я не хочу изучать Java, я хочу большую зарплату»
-*/
 
 public class Solution
 {
