7bd13f5dd22ea213a54b26f32c6dc8e57aae8df1
persey86 <persey86@ukr.net>
2016-09-03 23:04:28 +0300

    Solution for situation with incorrect value has added

--- src/prove/Factorial.java
+++ src/prove/Factorial.java
@@ -1,17 +1,18 @@
 package prove;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * Created by Anastasia on 11.08.2016.
+ *
  */
 public class Factorial
 {
     public static void main(String[] args) throws Exception
     {
         BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter your factorial");
+        System.out.println("Enter your factorial number");
         String n = sc.readLine();
         int N = Integer.parseInt(n);
         if (N>0){
@@ -22,11 +23,9 @@ public class Factorial
                 i = i * k;
                 k++;
             }
-            System.out.print("Resault is: " + i);}
-            else{
-            System.out.println("Error factorial value, reenter it again! ");
-            System.exit(-1);
+            System.out.println("Resault is: " + i);
         }
-
-        }
+            else
+            throw new IOException("Inserted Error value of 'n!' ! ");
+    }
 }
