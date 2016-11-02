d0be21384b33926082359c6f4e722fbe639bba52
persey86 <persey86@ukr.net>
2016-11-03 00:44:59 +0200

    Method "Search" and "Compare" in HasMap has been realized

---
+++ src/prove/Collections/AddAndDeleteElemToSet.java
@@ -0,0 +1,38 @@
+package com.javarush.test.level08.lesson08.task02;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/* Удалить все числа больше 10
+Создать множество чисел(Set<Integer>), занести туда 20 различных чисел.
+Удалить из множества все числа больше 10.
+*/
+
+public class AddAndDeleteToSetConIterator
+{
+    public static void main(String[] args)
+    {
+        System.out.println(createSet());
+    }
+    public static HashSet<Integer> createSet()
+    {
+        //напишите тут ваш код
+Set<Integer> set = new HashSet<Integer>();
+        for (int i=0;i<20;i++){
+            set.add(i);
+        }
+        return (HashSet<Integer>) set;
+    }
+
+    public static HashSet<Integer> removeAllNumbersMoreThan10(HashSet<Integer> set)
+    {
+        //напишите тут ваш код
+        Iterator<Integer> iterator = set.iterator();
+        while (iterator.hasNext()){
+            Integer digit = iterator.next();
+            if (digit>10) iterator.remove();
+        }
+        return set;
+}
+    }
