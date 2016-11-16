1bcab03faf9f08c291cd362b8d1692d189587f58
persey86 <persey86@ukr.net>
2016-11-16 23:58:01 +0200

    Template which shows how functionals Array with String

---
+++ src/prove/Arrays/ArrayListTempl.java
@@ -0,0 +1,44 @@
+package com.javarush.test.level10.lesson11.home08;
+
+import java.util.ArrayList;
+
+/* Массив списков строк
+Создать массив, элементами которого будут списки строк. Заполнить массив любыми данными и вывести их на экран.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String>[] arrayOfStringList =  createList();
+        printList(arrayOfStringList);
+    }
+
+    public static ArrayList<String>[] createList()
+    {
+        ArrayList<String>[] list = new ArrayList[3];
+        for (int i = 0; i < list.length; i++) {
+            list[i] = new ArrayList<String>();
+            list[i].add("Mama");
+            list[i].add("mila");
+            list[i].add("ramu");
+        }
+        for (ArrayList<String> text: list) {
+            text = new ArrayList<String>();
+            text.add("text");
+        }
+
+        return list;
+    }
+
+    public static void printList(ArrayList<String>[] arrayOfStringList)
+    {
+        for (ArrayList<String> list: arrayOfStringList)
+        {
+            for (String s : list)
+            {
+                System.out.println(s);
+            }
+        }
+    }
+}
