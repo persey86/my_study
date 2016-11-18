a7462bdce48767b756ef7ccd75f7fdfd9b7be895
persey86 <persey86@ukr.net>
2016-11-18 22:52:48 +0200

    Safety get elements from array .

--- src/prove/Collections/SafetyGetFromArray.java
+++ src/prove/Collections/SafetyGetFromArray.java
@@ -11,35 +11,35 @@ int safeGetElement(ArrayList<Integer> list, int index, int defaultValue)
 Если в процессе получения элемента возникло исключение, его нужно перехватить, и метод должен вернуть defaultValue.
 */
 
-public class Solution
-{
-    public static void main(String[] args) throws Exception
-    {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+ public class Solution
+ {
+     public static void main(String[] args) throws Exception
+     {
+         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for (int i = 0; i < 20; i++)
-        {
-            int x = Integer.parseInt(reader.readLine());
-            list.add(x);
-        }
+         ArrayList<Integer> list = new ArrayList<Integer>();
+         for (int i = 0; i < 20; i++)
+         {
+             int x = Integer.parseInt(reader.readLine());
+             list.add(x);
+         }
 
-        System.out.println(safeGetElement(list, 5, 1));
-        System.out.println(safeGetElement(list, 20, 7));
-        System.out.println(safeGetElement(list, -5, 9));
-    }
+         System.out.println(safeGetElement(list, 5, 1));
+         System.out.println(safeGetElement(list, 20, 7));
+         System.out.println(safeGetElement(list, -5, 9));
+     }
 
-    public static int safeGetElement(ArrayList<Integer> list, int index, int defaultValue)
-    {
-            try
-            {
-               return list.get(index);
-            }
-            catch (Exception e)
-            {
-                return defaultValue;
-            }
-        }
-    }
+     public static int safeGetElement(ArrayList<Integer> list, int index, int defaultValue)
+     {
+         try
+         {
+             return list.get(index);
+         }
+         catch (Exception e)
+         {
+             return defaultValue;
+         }
+     }
+ }
 
 
