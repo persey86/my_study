258a1bc2d73bd48b5f6d75e1b832407c2e878118
persey86 <persey86@ukr.net>
2016-11-29 23:51:31 +0200

    Default pattern of simple button

---
+++ src/prove/GUI/SimpleGui1.java
@@ -0,0 +1,21 @@
+package proove;
+
+import javax.swing.*;
+
+/**
+ * Created by Anastasia on 29.11.2016.
+ */
+public class SimpleGui1 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JButton button = new JButton("click me");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //for close frame
+
+        frame.getContentPane().add(button);  // add button on the pannel of frame
+
+        frame.setSize(300, 300); // size of the frame in px
+
+        frame.setVisible(true);  // make frame active (allow to show the frame)
+    }
+}
