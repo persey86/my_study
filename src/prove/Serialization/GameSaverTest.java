1e25e4f7878d7af624db0222ad7466a73ca5b19d
persey86 <persey86@ukr.net>
2016-12-09 00:12:20 +0200

    Algoritm "Search min element" in ArrayList has been realized

--- src/prove/Serialization/GameSaverTest.java
+++ src/prove/Serialization/GameSaverTest.java
@@ -1,7 +1,5 @@
 package Serialization;
 
-import java.io.*;
-
 /**
  * Created on 08.12.2016.
  */
@@ -9,6 +7,7 @@ public class GameSaverTest
 {
     public static void main(String[] args)
     {
+        //There is a few "hero"
         GameCharacter one = new GameCharacter(50, "Эльф", new String[] {"лук", "меч", "кастет"});
         GameCharacter two = new GameCharacter(200, "Тролль", new String[] {"голые руки", "большой топор"});
         GameCharacter three = new GameCharacter(120, "Маг", new String[] {"заклинания", "невидимость"});
@@ -27,7 +26,7 @@ public class GameSaverTest
         three = null;
 
         try{
-            ObjectInputStream is = new ObjectInputStream(new FileInputStream("Game.ser"));
+            ObjectInputStream is = new ObjectInputStream(new FileInputStream("Game.ser"));  // Here we've read from file
             GameCharacter oneRestore = (GameCharacter) is.readObject();
             GameCharacter twoRestore = (GameCharacter) is.readObject();
             GameCharacter threeRestore = (GameCharacter) is.readObject();
