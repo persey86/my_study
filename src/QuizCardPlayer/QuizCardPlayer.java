8d202afe15945c11b256a5473d0395d7a3406902
persey86 <persey86@ukr.net>
2016-12-15 23:56:12 +0200

    Simulator "Flash Card" with three classes: QuizCardBuilder (GUI), QuizCardPlayer (slider), QuizCard (class which contains and divide dates to cards). - GUI has been added with menu bar to slider

---
+++ src/QuizCardPlayer/QuizCardPlayer.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+/**
+ * Created on 15.12.2016.
+ */
+public class QuizCardPlayer {
+    private JTextArea display;
+    private JTextArea answer;
+    private ArrayList<QuizCard> cardList;
+    private QuizCard currentCard;
+    private int currentCardIndex;
+    private JFrame frame;
+    private JButton nextButton;
+    private boolean isShowAnswer;
+
+    public static void main(String[] args) {
+        QuizCardPlayer reader = new QuizCardPlayer();
+        reader.go();
+    }
+    public void go(){
+        frame = new JFrame("Quiz Card Player");
+        JPanel mainPanel = new JPanel();
+        Font bigFont = new Font("sanserif", Font.BOLD, 24);
+
+        display = new JTextArea(10,20);
+        display.setFont(bigFont);
+
+        display.setLineWrap(true);
+        display.setEditable(false);
+
+        JScrollPane qScroller = new JScrollPane(display);
+        qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        
+    }
+}
